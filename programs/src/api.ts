f7278e0d3418a8af2546c6d8c1770cc4b304e4cf
Jaden Brooklyn Lucas <26195291@sun.ac.za>
2025-01-17 19:10:44 +0200

    made changes towards database functions

--- programs/src/api.ts
+++ programs/src/api.ts
@@ -1,11 +1,14 @@
 import  express from "express";
+import { DatabaseSystem } from "./database";
 
 const app = express();
+const databaseSystem = new DatabaseSystem();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, Typescript + Node.js + Express!');
 })
+
 
 app.listen(port, () => {
     console.log('Server is running');
--- programs/src/database.ts
+++ programs/src/database.ts
@@ -22,31 +22,6 @@ const requestHandler = async (): Promise<any> => {
     }
 }
 
-// database functions
-const insertUser = async (username:string, email:string, password:string) : Promise<any> => {
-    let saltRounds: number = 10;
-    const encPassword: string = await bcrypt.hash(password, saltRounds);
-    const result = await sql('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id', [username, email, encPassword], {fullResults: true})
-    console.log(result.rows[0].id);
-}
-
-const insertNote = async (userId:number, name:string, content:string) : Promise<any> => {
-    try {
-        const result = await sql('INSERT INTO notes (name, content) VALUES ($1, $2) RETURNING id', [name, content]);
-        const noteId:number = result[0].id;
-        console.log(noteId)
-
-        await sql('INSERT INTO user_notes (user_id, note_id) VALUES ($1, $2)', [userId, noteId])
-    } catch (error:any) {
-        console.error(error);
-    }
-}
-
-
-const updateUser = async (id:number, obj: {username?: string, email?: string, password?:string}) : Promise<any> => {
-
-}
-
 
 // complex functions
 
@@ -72,29 +47,7 @@ const login = async (obj: {username?: string, email?: string}, password:string) 
     }
 }
 
-// token functions
 
-const generateToken = (id: number):string => {
-    if (JWT_SECRET) {
-        const token:string = jwt.sign({userId: id}, JWT_SECRET, {expiresIn: '1h'})
-        return token;
-    } else {
-        console.error("error with jwt secret token");
-        return '';
-    }
-}
-
-const verifyToken = (token: string) => {
-    if (JWT_SECRET) {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        console.log(decoded);
-    }
-}
-
-const viewAllUsers = async() : Promise<any> => {
-    const result = await sql('SELECT * FROM users');
-    return result;
-}
 
 // requestHandler().then((res) => {
 //     console.log(res)
@@ -104,7 +57,7 @@ export class DatabaseSystem {
     SQL: NeonQueryFunction<false, false>
 
     constructor() {
-        const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, JWT_SECRET } = process.env;
+        const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
         const url: string = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`;
 
         this.SQL = neon(url);
@@ -146,17 +99,41 @@ export class DatabaseSystem {
             throw error('username is empty')
         } else {
             const result: any = await this.SQL('SELECT id FROM users WHERE username = $1', [username]);
-            
+            const id: number = result[0].id;
+
+            const notesList: any = await this.SQL('SELECT id, name, content FROM notes INNER JOIN user_notes ON user_notes.note_id = notes.id WHERE user_id = $1', [id]);
+            console.log(notesList);
         }
     }
+
+
 }
 
-class tokenHandler {
-    createToken = generateToken;
-    decryptToken = verifyToken;
-}
+export class TokenHandler {
+    jwt_key:any
 
+    constructor() {
+        this.jwt_key = process.env.JWT_SECRET;
+    }
+
+    verifyToken = (token: string) => {
+        if (JWT_SECRET) {
+            const decoded = jwt.verify(token, JWT_SECRET);
+            console.log(decoded);
+        }
+    }
+
+    generateToken = (id: number):string => {
+        if (JWT_SECRET) {
+            const token:string = jwt.sign({userId: id}, JWT_SECRET, {expiresIn: '1h'})
+            return token;
+        } else {
+            console.error("error with jwt secret token");
+            return '';
+        }
+    }   
+}
 
 const databaseSystem = new DatabaseSystem();
-databaseSystem.insertUser('test9', 'test9password', 'test9@email.com');
+databaseSystem.get_notes_from_user('test2')
 // const databaseSystem = new DatabaseSystem();
